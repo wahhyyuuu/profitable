5726211e89a3604af99b0e63df4ca8dace03fb4c
5thphase <ridhme2607@gmail.com>
2021-07-09 06:27:16 +0700

    Fix Transaksi Migrate

--- app/Database/Migrations/2021-07-08-222016_Transaksi.php
+++ app/Database/Migrations/2021-07-08-222016_Transaksi.php
@@ -30,6 +30,11 @@ class Transaksi extends Migration
 				'constraint' => '50',
 				'null' => true,
 			],
+			'status' => [
+				'type' => 'ENUM',
+				'constraint' => ['success', 'pending', 'rejected'],
+				'default' => 'pending',
+			],
 			'created_at' => [
 				'type' => 'DATETIME',
 				'null' => true,
